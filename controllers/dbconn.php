a7a2031d3e1174b9484cf84a460be5d8edf75df3
Patricio-Bustos <patr.bustos@duocuc.cl>
2023-08-01 20:45:00 -0400

    Cambios Conexión
    Actualizacion metodos de conexión a bd y global vars

--- controllers/dbconn.php
+++ controllers/dbconn.php
@@ -5,64 +5,43 @@ include 'global_vars.php';
 
 function conectarDB($nConn) {
     
-    global $host, $puerto, $avn, $consumer, $entel, $forum, $globalvida, $rutasmaipo, $salcobran, $santander, $abcdin;
+    global $conexiones;
 
     switch ($nConn) {
         case 1:
-            $usuario = $avn['usuario'];
-            $contrasena = $avn['contrasena'];
-            $base_datos = $avn['base_datos'];
+            $connString = $conexiones['avn'];
             break;
         case 2:
-            $usuario = $consumer['usuario'];
-            $contrasena = $consumer['contrasena'];
-            $base_datos = $consumer['base_datos'];
+            $connString = $conexiones['consumer'];
             break;
         case 3:
-            $usuario = $entel['usuario'];
-            $contrasena = $entel['contrasena'];
-            $base_datos = $entel['base_datos'];
+            $connString = $conexiones['entel'];
             break;
         case 4:
-            $usuario = $forum['usuario'];
-            $contrasena = $forum['contrasena'];
-            $base_datos = $forum['base_datos'];
+            $connString = $conexiones['forum'];
             break;
         case 5:
-            $usuario = $globalvida['usuario'];
-            $contrasena = $globalvida['contrasena'];
-            $base_datos = $globalvida['base_datos'];
+            $connString = $conexiones['globalvida'];
             break;
         case 6:
-            $usuario = $rutasmaipo['usuario'];
-            $contrasena = $rutasmaipo['contrasena'];
-            $base_datos = $rutasmaipo['base_datos'];
+            $connString = $conexiones['rutasmaipo'];
             break;
         case 7:
-            $usuario = $salcobran['usuario'];
-            $contrasena = $salcobran['contrasena'];
-            $base_datos = $salcobran['base_datos'];
+            $connString = $conexiones['salcobran'];
             break;
         case 8:
-            $usuario = $santander['usuario'];
-            $contrasena = $santander['contrasena'];
-            $base_datos = $santander['base_datos'];
+            $connString = $conexiones['santander'];
             break;
         case 9:
-            $usuario = $abcdin['usuario'];
-            $contrasena = $abcdin['contrasena'];
-            $base_datos = $abcdin['base_datos'];
+            $connString = $conexiones['abcdin'];
             break;
         default:
-            $usuario = '';
-            $contrasena = '';
-            $base_datos = '';
+            $connString = '';
             break;
     }
 
+    $conn = pg_connect($connString);
 
-    $conn = pg_connect("host=" . $host . " port=" . $puerto . " dbname=" . $base_datos . " user=" . $usuario . " password=" . $contrasena);
-    
     if (!$conn) {
         die("Error al conectar a la base de datos: " . pg_last_error());
     }
