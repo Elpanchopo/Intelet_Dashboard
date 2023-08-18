225f91b300893acbdf4a7cb00cd7bb027d5d1354
Elpanchopo <101227418+Elpanchopo@users.noreply.github.com>
2023-08-18 17:14:52 -0400

    Update avn_fx.php
    se agregan funciones para mostrar resultados de gestiones.

--- models/avn_fx.php
+++ models/avn_fx.php
@@ -28,3 +28,48 @@ function getTablaTGDiarioSumTotal(){
     $query = "SELECT sum(ct) as ct, sum(te) as te, sum(nc) as nc, sum(cp) as cp, sum(total) as total from v_tabla_tipogestion_ejec_diario";
     return getJsonData($query, 1);
 }
+
+function getTotalGestiones(){
+    $query = "SELECT COUNT(pt.rut) AS "rut total" FROM planificacion_tag pt" 
+}
+
+function getGestionesTotalesMes(){
+    $query = "SELECT
+    c2
+    FROM (
+        SELECT 
+            (SELECT COUNT(pt.rut) AS "rut total" FROM planificacion_tag pt) AS c1,
+            (SELECT COUNT(DISTINCT g.rut_num) AS "tocados" 
+            FROM gestiones g 
+            WHERE (g.fecha_gestion >= DATE_TRUNC('month', CURRENT_DATE) AND g.fecha_gestion < CURRENT_DATE) 
+            AND ranking = 'CT') AS c2,
+            COUNT(*) AS cnt_dias
+        FROM (
+            SELECT DISTINCT fecha_gestion::date
+            FROM gestiones g
+            WHERE g.fecha_gestion >= DATE_TRUNC('month', CURRENT_DATE) AND g.fecha_gestion < CURRENT_DATE
+        ) AS tmp2
+    ) AS tmp;"
+    }
+
+
+//  Query datos a presentar.  
+SELECT
+c1,
+c2,
+(c2 / cnt_dias) AS c3,
+ROUND((c2 / c1::numeric) * 100, 2) || '%' AS c4
+FROM (
+SELECT 
+    (SELECT COUNT(pt.rut) AS "rut total" FROM planificacion_tag pt) AS c1,
+    (SELECT COUNT(DISTINCT g.rut_num) AS "tocados" 
+     FROM gestiones g 
+     WHERE (g.fecha_gestion >= DATE_TRUNC('month', CURRENT_DATE) AND g.fecha_gestion < CURRENT_DATE) 
+     AND ranking = 'CT') AS c2,
+    COUNT(*) AS cnt_dias
+FROM (
+    SELECT DISTINCT fecha_gestion::date
+    FROM gestiones g
+    WHERE g.fecha_gestion >= DATE_TRUNC('month', CURRENT_DATE) AND g.fecha_gestion < CURRENT_DATE
+) AS tmp2
+) AS tmp;
