bfb6b1ca7817819e6d2bc2634864c3e14f93984e
Elpanchopo <101227418+Elpanchopo@users.noreply.github.com>
2023-09-11 22:27:47 -0300

    Update abcdin_fx.php
    Actualizacion del repositorio.

--- models/abcdin_fx.php
+++ models/abcdin_fx.php
@@ -30,6 +30,88 @@ function getTablaTGDiarioSumTotal9(){
     return getJsonData($query, 9);
 };
 
+function getTablaEjecHoraXCampana($nom_campana){
+    $query = "SELECT a1.ejecutivo,
+    a1.c_9,
+    a1.c_10,
+    a1.c_11,
+    a1.c_12,
+    a1.c_13,
+    a1.c_14,
+    a1.c_15,
+    a1.c_16,
+    a1.c_17,
+    a1.c_18,
+    a1.c_19,
+    a1.c_9 + a1.c_10 + a1.c_11 + a1.c_12 + a1.c_13 + a1.c_14 + a1.c_15 + a1.c_16 + a1.c_17 + a1.c_18 + a1.c_19 AS total
+   FROM ( SELECT initcap(split_part(gestiones.ejecutivo::text, '-'::text, 2)) AS ejecutivo,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 9 THEN 1
+                    ELSE 0
+                END) AS c_9,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 10 THEN 1
+                    ELSE 0
+                END) AS c_10,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 11 THEN 1
+                    ELSE 0
+                END) AS c_11,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 12 THEN 1
+                    ELSE 0
+                END) AS c_12,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 13 THEN 1
+                    ELSE 0
+                END) AS c_13,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 14 THEN 1
+                    ELSE 0
+                END) AS c_14,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 15 THEN 1
+                    ELSE 0
+                END) AS c_15,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 16 THEN 1
+                    ELSE 0
+                END) AS c_16,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 17 THEN 1
+                    ELSE 0
+                END) AS c_17,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 18 THEN 1
+                    ELSE 0
+                END) AS c_18,
+            sum(
+                CASE
+                    WHEN to_char(gestiones.fecha_gestion, 'HH24'::text)::integer = 19 THEN 1
+                    ELSE 0
+                END) AS c_19
+           FROM gestiones
+          WHERE gestiones.fecha_gestion >= CURRENT_DATE::timestamp without time zone 
+          AND gestiones.fecha_gestion <= ((CURRENT_DATE || ' 23:59:59'::text)::timestamp without time zone) 
+          AND gestiones.medio_contacto::text = 'TELEFONICO'::text AND gestiones.ejecutivo::text <> '200-SUPERVISOR'::TEXT 
+          AND gestiones.campana = '$nom_campana'
+          GROUP BY gestiones.ejecutivo) a1
+  ORDER BY a1.ejecutivo;";
+  return getJsonData($query,9);
+}
+
+
+
 // Funcion que consulta la vista con los valores de las tarjetas y retorna un ARRAY para asignarlo en la vista
 function getCardsData() {
     $query = "SELECT * FROM v_cards_test";
